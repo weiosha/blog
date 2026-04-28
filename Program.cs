29fd7099ade52584aa12d35e8f0e27df5f6a5e12
Nicole Grace <nicoleverzosaosorio@gmail.com>
2026-04-29 04:54:53 +0800

    feat(program.cs):implement routing

--- Program.cs
+++ Program.cs
@@ -1,29 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add MVC services
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+// Enable static files (css, js, etc.)
+app.UseStaticFiles();
 
-app.UseHttpsRedirection();
 app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
-
+// Default route configuration
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{controller=Blog}/{action=Index}/{id?}");
 
 app.Run();
